5c1e713f605b7b4acf2bdc65cf38738d963e07f0
riteshjillewad <riteshjillewad292@gmail.com>
2026-07-17 14:08:09 +0530

    array utility functions

---
+++ modules/array_utils/array_utils.py
@@ -0,0 +1,17 @@
+###########################################################################################
+# Name        : array_utils.py
+# Description : Utility functions for performing various array-based operations.
+# Author      : Ritesh Jillewad
+# Date        : 01-07-2026
+# Version     : 1.0.0
+###########################################################################################
+
+"""
+array_utils.py
+-------------------------------------------------------------------------------------------
+A collection of utility functions for array-based operations.
+-------------------------------------------------------------------------------------------
+
+Functions:
+
+"""
---
+++ modules/array_utils/demo.py
@@ -0,0 +1,16 @@
+###########################################################################################
+# Name        : demo.py
+# Description : Client-side to test the implemented functions
+# Author      : Ritesh Jillewad
+# Date        : 17-07-2026
+# Version     : 1.0.0
+###########################################################################################
+
+from array_utils import *
+
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    main()
